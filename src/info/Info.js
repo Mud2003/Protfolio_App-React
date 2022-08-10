1dc62cfdbf9cb09716b51f744a1c1bd2053d6f50
Muditha Madusith <bmmadusith2003@gmail.com>
2022-08-10 12:59:47 +0530

    Add the files.

---
+++ src/info/Info.js
@@ -0,0 +1,136 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Muditha",
+    lastName: "Madusith",
+    initials: "mm", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a FrontEnd Web Developer",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in the SL'
+        },
+        {
+            emoji: "💼",
+            text: "Web Developer @Fiverr"
+        },
+        {
+            emoji: "📧",
+            text: "kabmmadusith2003@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://www.facebook.com/muditha.madusith",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://www.instagram.com/_bha.nu_/?hl=en",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com/Mud2003",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://www.linkedin.com/in/muditha-madusith-6830b0226/",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com/MudithaMadusith",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Hello! I'm Muditha. I'm currently BIT student at University of Colombo School of Computing. I'm currently work @Fiverr as a FrontEnd Web Developer.",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
+            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'travelling',
+            emoji: '💆‍♂️​'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Project 1",
+            live: "#", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "#", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Project 2",
+            live: "#",
+            source: "#",
+            image: mock1
+        },
+        {
+            title: "Project 3",
+            live: "#",
+            source: "#",
+            image: mock1
+        },
+        {
+            title: "Project 4",
+            live: "#",
+            source: "#",
+            image: mock1
+        },
+        {
+            title: "Project 5",
+            live: "#",
+            source: "#",
+            image: mock1
+        }
+    ]
+}
